8efe09b1d9f80c04828a1fb03708f56d1dc70d8b
aayusx <159042369+aayusx@users.noreply.github.com>
2025-11-01 22:09:55 +0545

    Fixed filename casing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import CompleteProfile from "./pages/CompleteProfile";
-import Landing from "./pages/Landing";
-import Login from "./pages/Login";
-import Profile from "./pages/Profile.jsx";
-import Signup from "./pages/Signup";
+import CompleteProfile from "./pages/completeprofile.jsx";
+import Landing from "./pages/landing.jsx";
+import Login from "./pages/login.jsx";
+import Profile from "./pages/profile.jsx";
+import Signup from "./pages/signup.jsx";
 
 export default function App() {
   return (
